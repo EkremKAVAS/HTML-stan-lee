2288176adced0d1e6cd620731b1a1849fb0e58d6
ekrem <ekremkavas2012@gmail.com>
2024-03-20 12:38:56 +0300

    run

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const students = 43000;
+  const nameSurname = "Ekrem Cenkeri";
+  const dogruMu = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      {" "}
+      <h1>ad,soyadı :{nameSurname}</h1>
+      <p>ekrem</p>
+      <p>{students}</p>
+      {/* değişkenın değeri için süslü prantez  */}
+      <input type="text"/>
+      <input type="date"/>
+      {dogruMu ? students : <p>false değerinini yazdı</p>}
     </div>
+    </>
   );
 }
-
+//yanyana h1 p yazarsan kod patlar kapsayıcı etıket ister bunu yerıne react frag <> </> attığınızda sorun kalkacak
 export default App;
+//html kodlarını js ıcınde yazmaya sağlar jsx
