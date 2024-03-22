4c5b412b2abd26acd3e85f7d2ebf25683c9d6252
ekrem <ekremkavas2012@gmail.com>
2024-03-22 12:27:41 +0300

    run

--- rc02_comp-jsx/props/src/App.js
+++ rc02_comp-jsx/props/src/App.js
@@ -1,18 +1,19 @@
-import Cat from "./components/cat"
+import Cat from "./components/Cat";
 //companetes cat içine gir ordan export edilmiş Cat i import et
 
 //? Component
 function App() {
   //? JS kodu
-  const deneme = "REACT"
+  const deneme = "REACT";
 
   return (
     //! JSX (HTML- JS'nin birleşmiş hali)
     <div className="App">
-      <Cat/>
-     
+      <Cat />
+      <Cat />
+      <Cat />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- rc02_comp-jsx/props/src/components/cat.jsx
+++ rc02_comp-jsx/props/src/components/cat.jsx
@@ -1,14 +1,21 @@
 //!COMPANENTLER
 
-const Cat = () =>{
-    return(
-       //*mutlaka kapsayıcı parent elemente sahıp olmalı
-       <div>
-        <img src="" alt="" />
-         <p>Color:</p>
-         <hr />
-         </div>
-    )
-}
+import { Name } from "../nameComp/Name";
+//named exportun yazım seklı süslü içinde Aynı isimle kullanılır
+
+const Cat = () => {
+  return (
+    //*mutlaka kapsayıcı parent elemente sahıp olmalı
+    <div>
+        <Name/>
+      <img
+        src="https://pixabay.com/tr/photos/kedi-kedi-yavrusu-a%C4%9Fa%C3%A7-merakl%C4%B1-1647775/"
+        alt="" height="300px"  widht ="250px"
+      />
+      <p>Color:</p>
+      <hr />
+    </div>
+  );
+};
 
 export default Cat; //export yaparak kapıyı açtık ıstedıgımız yerde kullanabılırız
